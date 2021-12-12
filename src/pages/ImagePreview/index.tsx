6447749a62e2da500eec5c889acd579918fdd2f6
pedrohenriquebraga <gamesantos7@gmail.com>
2021-12-12 10:08:28 -0300

    Fix image preview zoom

--- src/pages/ImagePreview/index.tsx
+++ src/pages/ImagePreview/index.tsx
@@ -1,21 +1,22 @@
 import React, { useEffect } from "react";
 import ReactNative, { Dimensions } from "react-native";
 import Header from "../../components/Header";
-import ImageZoom from "react-native-image-pan-zoom"
+import ImageZoom from "react-native-image-pan-zoom";
 import { Container, ImageContainer, Image } from "./styles";
 import { useRoute } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
 import { HeaderButton } from "../../components/Header/styles";
 import { useCallback } from "react";
 import { LinkUtils } from "../../utils/link";
-import { useImageDimensions } from "@react-native-community/hooks"
+import { useImageDimensions } from "@react-native-community/hooks";
+import Loading from "../../components/Loading";
 
 const ImagePreview = () => {
   const linkUtils = new LinkUtils();
 
   const route = useRoute();
   const { name, url } = route.params as { name: string; url: string };
-  const { dimensions } = useImageDimensions({ uri: url })
+  const { dimensions } = useImageDimensions({ uri: url });
 
   useEffect(() => {
     ReactNative.Image.prefetch(url);
@@ -24,6 +25,8 @@ const ImagePreview = () => {
   const downloadFile = useCallback(async () => {
     await linkUtils.openLink(url);
   }, [name, url]);
+
+  if (!dimensions) return <Loading />;
 
   return (
     <>
@@ -35,15 +38,19 @@ const ImagePreview = () => {
       <Container>
         <ImageContainer>
           <ImageZoom
-            minScale={1}
-            maxScale={3}
-            imageWidth={dimensions?.width}
-            imageHeight={dimensions?.height}
+            imageWidth={Dimensions.get("screen").width}
+            imageHeight={Dimensions.get("screen").height}
             cropWidth={Dimensions.get("screen").width}
             cropHeight={Dimensions.get("screen").height}
+            enableDoubleClickZoom={true}
             useNativeDriver={true}
           >
-            <Image source={{ uri: url }} resizeMode="center" />
+            <Image
+              source={{ uri: url }}
+              width={dimensions.width}
+              height={dimensions.height}
+              resizeMode="center"
+            />
           </ImageZoom>
         </ImageContainer>
       </Container>
