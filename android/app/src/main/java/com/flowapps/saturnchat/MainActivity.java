02fcde57f8bbaea2e6b19377612b15860c4a2764
pedrohenriquebraga <gamesantos7@gmail.com>
2022-12-24 00:45:39 -0300

    Upgrade Expo 45 to 47

--- android/app/src/main/java/com/flowapps/saturnchat/MainActivity.java
+++ android/app/src/main/java/com/flowapps/saturnchat/MainActivity.java
@@ -61,4 +61,10 @@ public class MainActivity extends ReactActivity {
     // because it's doing more than {@link Activity#moveTaskToBack} in fact.
     super.invokeDefaultOnBackPressed();
   }
+
+  protected boolean isConcurrentRootEnabled() {
+    // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+    // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+    return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+  }
 }
