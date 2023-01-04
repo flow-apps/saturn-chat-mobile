5da63a92ab26f6ec240e00f5972e24e167de39cb
pedrohenriquebraga <gamesantos7@gmail.com>
2023-01-04 15:44:59 -0300

    Optimize code with babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: ["react-native-reanimated/plugin", "lodash"],
+    presets: ["babel-preset-expo", ["@babel/env", { targets: { node: 6 } }]],
     env: {
-      production: {},
+      production: {
+        plugins: ["transform-remove-console"], //removing consoles.log from app during release (production) versions
+      },
     },
   };
 };
