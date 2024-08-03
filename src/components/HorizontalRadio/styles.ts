d7acc2923aceba17761997799b4e0a04c40f48c9
pedrohenriquebraga <gamesantos7@gmail.com>
2024-08-03 17:30:52 -0300

    Fix color

--- src/components/HorizontalRadio/styles.ts
+++ src/components/HorizontalRadio/styles.ts
@@ -12,6 +12,7 @@ export const ButtonsContainer = styled.View`
 export const ButtonContainer = styled.TouchableOpacity`
   padding: 10px 25px;
   background-color: ${props => props.theme.colors.shape};
+  color: ${(props) => props.theme.colors.black};
   margin-right: 10px;
   border-radius: 25px;
   border: 1.5px solid ${props => props.theme.colors.shape};
