255fff1c7416052429f8632c37f2de15e96942a9
pedrohenriquebraga <pedrobraga693@gmail.com>
2026-08-29 15:33:21 -0300

    add keep data and edge-to-edge

--- android/app/src/main/java/com/flowapps/saturnchat/MainActivity.kt
+++ android/app/src/main/java/com/flowapps/saturnchat/MainActivity.kt
@@ -7,6 +7,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import androidx.activity.enableEdgeToEdge
 
 import expo.modules.ReactActivityDelegateWrapper
 
@@ -16,6 +17,7 @@ class MainActivity : ReactActivity() {
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
     setTheme(R.style.AppTheme);
+    enableEdgeToEdge()
     super.onCreate(null)
   }
 
